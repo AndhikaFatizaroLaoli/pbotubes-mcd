763b728ec0a8a6c820f6a72125f0ffa67a064f40
Andhika <andhikafatizarolaoli@gmail.com>
2026-07-08 11:09:32 +0700

    add logic login

---
+++ app/src/main/java/pbotubesmcd/AppConfig.java
@@ -0,0 +1,9 @@
+package pbotubesmcd;
+
+public class AppConfig {
+    public static final String DATABASE = "tubespbo-mcd";
+    public static final String PORT = "5432";
+    public static final String HOST = "localhost";
+    public static final String USER = "developer";
+    public static final String PASSWORD = "developer";
+}
--- app/src/main/java/pbotubesmcd/views/LoginView.java
+++ app/src/main/java/pbotubesmcd/views/LoginView.java
@@ -1,5 +1,7 @@
 package pbotubesmcd.views;
 
 public class LoginView {
-
+    public void run() {
+        // tampilin halaman loginnya di sini
+    }
 }
